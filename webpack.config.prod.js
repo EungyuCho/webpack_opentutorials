592aca8c94d5c011dbd916478ed3bd22c4f20dea
jay <cho2304@naver.com>
2021-04-01 17:03:00 +0900

    7: webpack plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "production",
   entry: {
@@ -18,4 +20,17 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "./index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./about.html",
+      filename: "./about.html",
+      chunks: ["about"],
+    }),
+  ],
 };
